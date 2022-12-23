f8fde34e3f85274caa6dc4ae45fdfa6720c1f8f4
Esraayase <121320357+Esraayase@users.noreply.github.com>
2022-12-23 14:28:10 -0800

    Add files via upload

---
+++ project 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Perfect numbers are :");
+            for (int i = n1; i <= n2; i++)
+            {
+                int j = 1;
+                int sum = 0;
+                while(j<i)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+                    j++;
+
+                }
+                if (sum == i)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
---
+++ project d.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = n1; i <= n2; i++)
+            {
+                int count = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0 && i != 1)
+
+                {
+                    Console.WriteLine("\n" + i);
+
+                }
+            }
+        }
+    }
+}
+
+
